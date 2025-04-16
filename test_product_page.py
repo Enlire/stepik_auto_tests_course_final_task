a38d22f5c5b7913cdd00ae7d88b002d80dedfb9a
Enlire <amandosova.adelina@gmail.com>
2025-04-16 16:01:15 +0300

    Перенос методов в BasePage

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 from selenium.webdriver.common.by import By
 import pytest
 import time
@@ -41,3 +42,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 	page.open()
 	page.click_on_add_to_basket_button()
 	page.success_message_should_disappear()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_login_page()
+	login_page = LoginPage(browser, browser.current_url)
+	login_page.should_be_login_page()
+	login_page.should_be_login_form()
+	login_page.should_be_register_form()
